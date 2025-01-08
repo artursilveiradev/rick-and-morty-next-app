ec20872e8cfc8b9539293846328ec2f267807998
Artur Silveira <artur.oliveira.silveira@gmail.com>
2025-01-08 17:43:05 -0300

    add Header component

---
+++ app/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="mb-8 py-6">
+      <h1 className="text-center font-orbitron text-3xl font-bold text-neon-green">
+        Rick and Morty characters
+      </h1>
+    </header>
+  );
+}
